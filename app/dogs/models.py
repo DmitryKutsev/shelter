a5a6967170e022d0c8d90f7d592777c43b162bd5
dmitrykucev <dmitrykucev@mail.ru>
2022-08-07 09:44:46 +0300

    added db service #7

--- app/dogs/models.py
+++ app/dogs/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class AvaliableDogs(models.Model):
+    name = models.CharField(max_length=50)
+    title = models.CharField(max_length=150)
+    age = models.CharField(max_length=5)
+    breed = models.CharField(max_length=50)
+    temperament = models.CharField(max_length=150)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
